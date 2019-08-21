f995c5600b4211dd5812547a195698d1858252e9
Sean Davis <sean.dev.davis@gmail.com>
2019-08-21 18:10:55 -0500

    first part of not hello, world

--- MyFirstConsoleApp/Program.cs
+++ MyFirstConsoleApp/Program.cs
@@ -7,41 +7,8 @@ namespace MyFirstConsoleApp
     {
         static void Main(string[] args)
         {
-            var myVariable = 456;
-            var myIntVariable = 123;
-
-            switch (myIntVariable)
-            {
-                case 1:
-                    Console.WriteLine("We'll never hit this code.");
-                    break;
-                case 2:
-                    Console.WriteLine("We'll hit this code.");
-                    break;
-                default:
-                    Console.WriteLine("We'll never hit this code.");
-                    break;
-            }
-
-            for (var i = 0; i < 5; i++)
-            {
-                Console.WriteLine($"We're on iteration {i}");
-            }
-
-            List<string> myStrings = new List<string>();
-            myStrings.Add("blerg");
-            myStrings.Add("blarg");
-            myStrings.Insert(1, "blirg");
-            myStrings.Remove("blirg");
-            myStrings.RemoveAt(1);
-
-
-            foreach (var myString in myStrings)
-            {
-                Console.WriteLine(myString);
-            }
-
-            Console.WriteLine(myVariable);5
+            Console.WriteLine("Welcome to Bangazon!");
+            Console.ReadKey();
         }
     }
 }
